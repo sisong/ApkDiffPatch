33168b74157669eb8940b96eb2754b6a3d65e02e
sisong <sisong@gmail.com>
2018-02-25 08:50:59 +0800

    初始化仓库；计划实现一个简单版本的支持v2签名的apk增量更新；

---
+++ apk-diff.cpp
@@ -0,0 +1,14 @@
+//  apk-diff.cpp
+//  ApkDiff
+//
+//  Created by housisong on 2018/2/24.
+//  Copyright © 2018年 housisong. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, ApkDiff!\n";
+    return 0;
+}
---
+++ apk-normalized.cpp
@@ -0,0 +1,17 @@
+//  apk-normalized.cpp
+//  ApkNormalized
+//
+//  Created by housisong on 2018/2/25.
+//  Copyright © 2018年 housisong. All rights reserved.
+//
+
+#include <iostream>
+//diff前对apk包进行规范化处理:
+//  输入apk文件(v1版签名),输出重新按固定压缩算法生成的新apk文件(v1签名会被保留,v2签名会被删除)
+//  (需要另外用Android工具对输出的apk文件执行v2签名)
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, ApkNormalized!\n";
+    return 0;
+}
