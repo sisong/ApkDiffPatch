f3e361beaf015245920d6c17b7589b85ce0c8d52
sisong <sisong@gmail.com>
2019-09-16 11:28:24 +0800

    update hdiffpatch;   APP_STL  from  c++_static to stlport_static (.so file size smaller)

--- builds/android_ndk_jni_mk/Application.mk
+++ builds/android_ndk_jni_mk/Application.mk
@@ -1,9 +1,9 @@
-APP_STL := c++_static
-APP_CPPFLAGS := -frtti -std=c++11 -fexceptions
+APP_PLATFORM := android-14
 APP_CFLAGS += -Wno-error=format-security
 APP_CFLAGS += -fvisibility=hidden -fvisibility-inlines-hidden
 APP_CFLAGS += -ffunction-sections -fdata-sections
 APP_LDFLAGS += -Wl,--gc-sections
-APP_PLATFORM := android-14
+APP_STL      := stlport_static
+APP_CPPFLAGS := -fexceptions
 APP_BUILD_SCRIPT := Android.mk
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
