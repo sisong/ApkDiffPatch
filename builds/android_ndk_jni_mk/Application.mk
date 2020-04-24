433aefdb4b42768f776cc53af4e30ce776710172
iwiniwin <iwiniwin@foxmail.com>
2020-04-24 15:53:52 +0800

    Set APP_STL to c++_static to solve the problem of stlport_static is no longer supported

--- builds/android_ndk_jni_mk/Application.mk
+++ builds/android_ndk_jni_mk/Application.mk
@@ -3,7 +3,7 @@ APP_CFLAGS += -Wno-error=format-security
 APP_CFLAGS += -fvisibility=hidden -fvisibility-inlines-hidden
 APP_CFLAGS += -ffunction-sections -fdata-sections
 APP_LDFLAGS += -Wl,--gc-sections
-APP_STL      := stlport_static
+APP_STL      := c++_static
 APP_CPPFLAGS := -fexceptions
 APP_BUILD_SCRIPT := Android.mk
 APP_ABI := armeabi-v7a arm64-v8a x86 x86_64
